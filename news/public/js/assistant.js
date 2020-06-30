6f70670f855f97f78be3d14687cc60e4e3bf6092
Jack Xu <xujack2001@gmail.com>
2020-06-30 14:09:43 -0400

    implemented sentiment sorting

--- news/public/js/assistant.js
+++ news/public/js/assistant.js
@@ -50,28 +50,89 @@ class Assistant {
   }
 }
 
-
-
-async function fetchNewsAndDisplay(query=null) {
-  var url = 'http://newsapi.org/v2/everything?' +
-      'qInTitle=AAPL&' +
-      'sortBy=popularity&' +
-      'excludeDomains=&' +
-      'apiKey=28201518fa35451294f5fb1498f79105';
-  var req = new Request(url);
-  fetch(req).then(response => response.json()).then(response =>{
-    var articles = response.articles;
-    var list = document.getElementById("news-list");
-    var articlesLength = Object.keys(articles).length;
-    console.log(articlesLength);
-    console.log(articles);
-    for(i=0;i<articlesLength;i++) {
-      var article = articles[i];
-      console.log(article);
-      console.log(article.title);
-      var newsTitle = document.createElement("li");
-      newsTitle.appendChild(document.createTextNode(article.title));
-      list.appendChild(newsTitle);
-    }
+async function getSentimentsAndSort(articles) {
+  //add articles to a json obj
+  var jsonObject = {};
+  jsonObject.documents = [];
+  for(i=0;i<articles.length;i++) {
+    var article = articles[i];
+    var docObject = {};
+    docObject.id = i+1;
+    docObject.language = "en";
+    docObject.text = article.name + ". " + article.description;
+    jsonObject.documents.push(docObject);
+  }
+  //make obj into json
+  var docJSON = JSON.stringify(jsonObject);
+  console.log(articles);
+  console.log(docJSON);
+  //get the sentiment values
+  var documents = {};
+  fetch("https://microsoft-text-analytics1.p.rapidapi.com/sentiment", {
+    "method": "POST",
+    "headers": {
+      "x-rapidapi-host": "microsoft-text-analytics1.p.rapidapi.com",
+      "x-rapidapi-key": "968db1c3aamsh38e8276ca5be196p1bab0ejsn0973e5df894d",
+      "content-type": "application/json",
+      "accept": "application/json"
+    },
+    "body": docJSON
+  }).then(response => response.json()).then(response => {
+    documents = response.documents;
   });
+  //now assign the scores to the actual articles
+  for(i=0;i<articles.length;i++) {
+    var doc = documents[i];
+    articles[i].positivity = doc.documentScores.positive - doc.documentScores.negative;
+  }
+  //now sort the articles and return them
+  console.log(typeof(articles));
+  articles = [].slice.call(articles).sort(function(a,b){
+    if (a.positivity == b.positivity) return 0;
+    return a.positivity > b.positivity ? 1 : -1;
+  });
+  return articles;
+}
+
+async function displayNews(sortBySentiment=true) {
+  var articles = fetchNews();
+  console.log("Below is articles from fetchNews function");
+  console.log(articles);
+  var sortedArticles,resolvedArticles;
+  //assign sentiment score to each article
+  articles.then(resolved => resolvedArticles = resolved);
+  console.log(resolvedArticles);
+  if (sortBySentiment) resolvedArticles = sortBySentiment(resolvedArticles);
+  //render articles
+  renderNews(resolvedArticles);
+}
+
+function fetchNews() {
+  var url = 'https://bing-news-search1.p.rapidapi.com/news/search?' +
+      'q=AAPL%20STOCK';
+  fetch(url, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+      "x-rapidapi-key": "968db1c3aamsh38e8276ca5be196p1bab0ejsn0973e5df894d",
+      "x-bingapis-sdk": "true"
+    }
+  }).then(response => response.json()).then(articles => {
+    var articles = articles.value;
+    console.log(typeof articles);
+    return articles;
+  });
+}
+
+async function renderNews(articles) {
+  var list = document.getElementById("news-list");
+  for(i=0;i<articles.length;i++) {
+    var article = articles[i];
+    var newsTitle = document.createElement("li");
+    newsTitle.appendChild(document.createTextNode(article.name));
+    var newsDescrip = document.createElement("dd");
+    newsDescrip.appendChild(document.createTextNode(article.description));
+    list.appendChild(newsTitle);
+    list.appendChild(newsDescrip);
+  }
 }
